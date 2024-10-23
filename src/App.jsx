5f5cb4541fb7790b131256477cf2544bcf7fbca2
obidyhasan <obidyhasan@gmail.com>
2024-10-23 15:23:37 +0600

    include OurRecipe secton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import OurRecipe from "./Components/OurRecipe";
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto px-5">
       <Navbar></Navbar>
       <Header />
+      <OurRecipe />
     </div>
   );
 }
---
+++ src/Components/OurRecipe.jsx
@@ -0,0 +1,14 @@
+function OurRecipe() {
+  return (
+    <div className="max-w-3xl mx-auto text-center my-16">
+      <h1 className="font-bold text-3xl">Our Recipes</h1>
+      <p className="text-sm mt-3">
+        Discover a variety of delicious, easy-to-make recipes in our collection.
+        From quick meals to indulgent treats, we’ve got something for every
+        taste and occasion!
+      </p>
+    </div>
+  );
+}
+
+export default OurRecipe;
